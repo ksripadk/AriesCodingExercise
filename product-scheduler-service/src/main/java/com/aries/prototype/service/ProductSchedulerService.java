bd6f34204c9d35111fc3d42738fa0301f0f97284
ksripadk <88186855+ksripadk@users.noreply.github.com>
2021-07-31 15:05:13 +0530

    Update ProductSchedulerService.java
    Added a service method.

--- product-scheduler-service/src/main/java/com/aries/prototype/service/ProductSchedulerService.java
+++ product-scheduler-service/src/main/java/com/aries/prototype/service/ProductSchedulerService.java
@@ -1,7 +1,27 @@
 package com.aries.prototype.service;
 
+import com.aries.prototype.model.Product;
+import com.aries.prototype.model.ProductRequest;
+import com.aries.prototype.utility.DateUtility;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductSchedulerService {
+
+    public List<Product> handleAddProducts(List<ProductRequest> productRequestList, List<Product> productsList)
+    {
+        for (ProductRequest productRequest : productRequestList) {
+            if(productRequest.getCustomerId() != null && productRequest.getProductName() != null &&
+                    productRequest.getDomain() != null && productRequest.getStartDate() != null) {
+
+                productsList.add(new Product(productRequest.getCustomerId(), productRequest.getProductName(),
+                        productRequest.getDomain(), productRequest.getStartDate(), productRequest.getDurationMonths(),
+                        DateUtility.getExpirationDate(productRequest.getStartDate(), productRequest.getDurationMonths())
+                ));
+            }
+        }
+        return productsList;
+    }
 }
