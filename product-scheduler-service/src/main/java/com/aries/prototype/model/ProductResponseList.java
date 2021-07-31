eddd8967710e30ee076c556a6445970bf8a87c30
ksripadk <88186855+ksripadk@users.noreply.github.com>
2021-07-31 15:41:45 +0530

    Add files via upload

---
+++ product-scheduler-service/src/main/java/com/aries/prototype/model/ProductResponseList.java
@@ -0,0 +1,16 @@
+package com.aries.prototype.model;
+
+import java.util.List;
+
+public class ProductResponseList {
+
+    List<ProductResponse> productResponseList;
+
+    public List<ProductResponse> getProductResponseList() {
+        return productResponseList;
+    }
+
+    public void setProductResponseList(List<ProductResponse> productResponseList) {
+        this.productResponseList = productResponseList;
+    }
+}
