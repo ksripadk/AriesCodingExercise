e4f20a7baf23629a105bcf2d73a6e283bed85184
ksripadk <88186855+ksripadk@users.noreply.github.com>
2021-07-31 15:37:24 +0530

    Update DateUtility.java

--- product-scheduler-service/src/main/java/com/aries/prototype/utility/DateUtility.java
+++ product-scheduler-service/src/main/java/com/aries/prototype/utility/DateUtility.java
@@ -2,7 +2,9 @@ package com.aries.prototype.utility;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class DateUtility {
 
@@ -16,6 +18,38 @@ public class DateUtility {
             e.printStackTrace();
         }
         c.add(Calendar.MONTH, durationMonths);  // number of months to add
+        date = sdf.format(c.getTime());
+        return date;
+    }
+
+    public static List<String> getEmailDates(String startDate, String expDate, String prodName)
+    {
+        List<String> dateList = new ArrayList<String>();
+        if (prodName.equals(ProductTypeConstants.DOMAIN)) {
+            dateList.add(getEmailDate(expDate, -2));
+        }
+        else if (prodName.equals(ProductTypeConstants.HOSTING)) {
+            dateList.add(getEmailDate(startDate, 1));
+            dateList.add(getEmailDate(expDate, -3));
+        }
+        else if (prodName.equals(ProductTypeConstants.PDOMAIN)) {
+            dateList.add(getEmailDate(expDate, -9));
+            dateList.add(getEmailDate(expDate, -2));
+        }
+
+        return dateList;
+    }
+
+    public static String getEmailDate(String date, int days)
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        try {
+            c.setTime(sdf.parse(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        c.add(Calendar.DATE, days);  // number of days to add
         date = sdf.format(c.getTime());
         return date;
     }
