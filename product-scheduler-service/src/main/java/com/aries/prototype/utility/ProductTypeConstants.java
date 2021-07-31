dc03ad6223d69b9fa636933813ff315c11dc3fca
ksripadk <88186855+ksripadk@users.noreply.github.com>
2021-07-31 15:36:23 +0530

    Add files via upload

---
+++ product-scheduler-service/src/main/java/com/aries/prototype/utility/ProductTypeConstants.java
@@ -0,0 +1,7 @@
+package com.aries.prototype.utility;
+
+public class ProductTypeConstants {
+    public static final String DOMAIN = "domain";
+    public static final String PDOMAIN = "pdomain";
+    public static final String HOSTING = "hosting";
+}
