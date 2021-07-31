e1cf8beafb9776a26b3cd2edf368c76e1d9dd76f
ksripadk <88186855+ksripadk@users.noreply.github.com>
2021-07-31 15:40:28 +0530

    Update ProductSchedulerService.java

--- product-scheduler-service/src/main/java/com/aries/prototype/service/ProductSchedulerService.java
+++ product-scheduler-service/src/main/java/com/aries/prototype/service/ProductSchedulerService.java
@@ -2,9 +2,12 @@ package com.aries.prototype.service;
 
 import com.aries.prototype.model.Product;
 import com.aries.prototype.model.ProductRequest;
+import com.aries.prototype.model.ProductResponse;
 import com.aries.prototype.utility.DateUtility;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -23,5 +26,25 @@ public class ProductSchedulerService {
             }
         }
         return productsList;
+    }
+
+    public List<ProductResponse> handleGetEmailScheduler(List<Product> productList)
+    {
+        List<ProductResponse> emailProdList = new ArrayList<ProductResponse>();
+        if(productList != null && productList.size() != 0) {
+            for (Product product : productList) {
+                if (product.getCustomerId() != null) {
+                    List<String> dateList = DateUtility.getEmailDates(product.getStartDate(), product.getExpirationDate(), product.getProductName());
+                    if (dateList.size() != 0 && dateList != null) {
+                        for (String date : dateList) {
+                            emailProdList.add(new ProductResponse(product.getCustomerId(), product.getProductName(),
+                                    product.getDomain(), date));
+                        }
+                    }
+                }
+            }
+        }
+        Collections.sort(emailProdList);
+        return emailProdList;
     }
 }
